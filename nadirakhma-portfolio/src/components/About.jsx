1aa8455671077691ba78ad6befe6eb36a115d077
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-11-16 20:25:26 +0700

    Refactor code structure About, with cv's

--- nadirakhma-portfolio/src/components/About.jsx
+++ nadirakhma-portfolio/src/components/About.jsx
@@ -1,4 +1,6 @@
 import Logo from '../assets/logo.png';
+import cvATS from '../assets/pdf/cv-ats.pdf';
+import cvCreative from '../assets/pdf/cv.pdf';  
 
 const About = () => {
   const stylishFont = "font-serif";
@@ -7,7 +9,7 @@ const About = () => {
   const HighlightText = ({ children }) => (
     <span className="relative inline-block">
       {children}
-      <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-indigo-300 opacity-50"></span>
+      <span className="absolute left-0 -bottom-0 w-full h-0.5 bg-[#FF6600] opacity-50"></span>
     </span>
   );
 
@@ -16,8 +18,8 @@ const About = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
         {/* Konten Teks */}
         <div>
-          <h2 className={`text-6xl ${stylishFont} text-white mb-8`}>About Me</h2>
-          <p className="text-lg text-indigo-200 leading-relaxed mb-10">
+          <h2 className={`text-6xl md:text-8xl font-stylish text-white mb-8`}>About Me</h2>
+          <p className="text-lg font-modern text-indigo-200 leading-relaxed mb-10">
             I am passionate about building{' '}
             <HighlightText>creative and user-centered digital experiences</HighlightText>{' '}
             through <HighlightText>Front-End Development, Graphic and UI Design</HighlightText>.
@@ -27,12 +29,23 @@ const About = () => {
           </p>
           
           <div className="flex gap-6">
-            <button className="px-8 py-3 rounded-full text-lg font-semibold text-white bg-indigo-700 hover:bg-indigo-600 transition-all duration-300 shadow-lg">
-              CV ATS
-            </button>
-            <button className="px-8 py-3 rounded-full text-lg font-semibold text-white bg-red-600 hover:bg-red-700 transition-all duration-300 shadow-lg">
-              CV Creative
-            </button>
+            <a 
+  href={cvATS}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="px-8 py-3 rounded-full text-lg font-semibold text-white bg-indigo-500 hover:bg-indigo-700 hover:scale-105 transition-all duration-300 shadow-lg"
+>
+  CV ATS
+</a>
+
+<a 
+  href={cvCreative}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="px-8 py-3 rounded-full text-lg font-semibold text-white bg-red-600 hover:bg-red-700 hover:scale-105 transition-all duration-300 shadow-lg"
+>
+  CV Creative
+</a>
           </div>
         </div>
 
