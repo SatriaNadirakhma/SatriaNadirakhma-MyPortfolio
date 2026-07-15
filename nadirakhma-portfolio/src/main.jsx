da0e677dfbf9d26af91bcace1b077b2d96028df1
SatriaNadirakhma <rakhmadanisatria@gmail.com>
2026-07-15 09:22:37 +0700

    new Speed-Insight for Vercel data visualization

--- nadirakhma-portfolio/src/main.jsx
+++ nadirakhma-portfolio/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@context/ThemeContext";
 import App from "@/App";
 import "@/index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
+      <SpeedInsights />
       <App />
     </ThemeProvider>
   </StrictMode>,
