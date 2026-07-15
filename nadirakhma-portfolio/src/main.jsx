a2f49f79a5f0cca6e7bbc6e74c08b909a77f5755
SatriaNadirakhma <rakhmadanisatria@gmail.com>
2026-07-15 09:23:58 +0700

    react chore speed insight

--- nadirakhma-portfolio/src/main.jsx
+++ nadirakhma-portfolio/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@context/ThemeContext";
 import App from "@/App";
 import "@/index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
